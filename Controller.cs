faf5018f8063fae389dcb2bde025b924d66ff660
Tope Adio <149920417+adiotope12@users.noreply.github.com>
2024-08-01 17:41:12 -0400

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,130 @@
+﻿using Api_Project.Models;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api_Project.Controllers
+{
+   
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        private static SecretClient _client = new SecretClient(vaultUri: new Uri("https://myserverstatuskeyvault12.vault.azure.net/"), credential: new DefaultAzureCredential());
+        private static KeyVaultSecret _secret = _client.GetSecret("ApiKeyThingy");
+        private static CosmosClient _cosmosDB = new CosmosClient(@"https://apicosmosdb12.documents.azure.com:443/", _secret.Value);
+        private static Microsoft.Azure.Cosmos.Container _Results = _cosmosDB.GetContainer("ApiStorage", "Results");
+        private readonly ILogger<Controller> _logger;
+        public Controller(ILogger<Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResponseResult>> Get(string id)
+        {
+            try
+            {
+                StorageObject readItem = await _Results.ReadItemAsync<StorageObject>(
+               id: id,
+               partitionKey: new PartitionKey(id));
+                return readItem.result;
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost("{id}/{Height}/{Weight}/{Age}/{Gender}/{Workouts_Per_Week}")]
+        public async Task<ActionResult<ResponseResult>> Post(string id, string Height, double Weight, int Age, string Gender,int Workouts_Per_Week)
+        {
+            try
+            {
+                var item = new StorageObject();
+                item.id = id;
+                item.person = new Person();
+                item.person.Height = Height;
+                item.person.Weight = Weight;
+                item.person.Age = Age;
+                item.person.Gender = Gender;
+                item.person.Workouts_Per_Week = Workouts_Per_Week;
+
+                item.result = item.CalculateResults(id, item.person);
+                await _Results.CreateItemAsync<StorageObject>(item, new PartitionKey(id));
+
+                return item.result;
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<StorageObject>> Put(string id, string? Height = null, double Weight = 0, int Age=0, string? Gender=null, int Workouts_Per_Week=0)
+        {
+            try
+            {
+                StorageObject readItem = await _Results.ReadItemAsync<StorageObject>(
+               id: id,
+               partitionKey: new PartitionKey(id));
+                if (Height != null)
+                {
+                    readItem.person.Height = Height;
+                }
+
+                if (Weight != 0)
+                {
+                    readItem.person.Weight = Weight;
+                }
+                if (Age != 0)
+                {
+                    readItem.person.Age = Age;
+                }
+                if (Gender != null)
+                {
+                    readItem.person.Gender = Gender;
+                }
+                if (Workouts_Per_Week != 0)
+                {
+                    readItem.person.Workouts_Per_Week = Workouts_Per_Week;
+                }
+
+                readItem.result = readItem.CalculateResults(id, readItem.person);
+                await _Results.ReplaceItemAsync<StorageObject>(readItem, id, new PartitionKey(id));
+                return readItem;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                await _Results.DeleteItemAsync<StorageObject>(id, new PartitionKey(id));
+                return Ok();
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+    }
+}
---
+++ Results.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Api_Project.Models
+{
+    public class Person
+    {
+        public string Height { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public int Workouts_Per_Week { get; set; }
+
+    }
+    public class ResponseResult
+    {
+        public string id { get; set; }
+        public double BMI { get; set; }
+        public int Maintenance_Calories{ get; set; }
+        public int carbs { get; set; }
+        public int fat { get; set; }
+        public int protein { get; set; }
+
+
+    }
+
+    public class StorageObject
+    {
+        public string id { get; set; }  
+        public Person person { get; set; }
+        public ResponseResult result { get; set; }
+        public ResponseResult CalculateResults(string id, Person person)
+        {
+            var Results = new ResponseResult();
+            Results.id = id;
+            var heightParts = person.Height.Split('.');
+
+            int feet = int.Parse(heightParts[0]);
+            int inches = int.Parse(heightParts[1]);
+
+            var heightCm = ((feet * 12) + inches) * 2.54;
+
+            double intensity;
+            if(person.Workouts_Per_Week == 0)
+            {
+                intensity = 1.2;
+            } else if (person.Workouts_Per_Week <= 3){
+                intensity = 1.375;
+            } else if (person.Workouts_Per_Week <= 5){
+                intensity = 1.55;
+            } else if (person.Workouts_Per_Week <= 7){
+                intensity = 1.725;
+            } else
+            {
+                throw new ArgumentException("Invalid Workouts Per Week");
+            }
+            switch (person.Gender.ToUpper())
+            {
+                case "MALE":
+                    Results.Maintenance_Calories = (int)Math.Round(((88.4 + (13.4 * (person.Weight/2.205))) + (4.8 * heightCm) - (5.68 * person.Age))*intensity);
+                    break;
+
+                case "FEMALE":
+                    Results.Maintenance_Calories = (int)Math.Round(((447.6 + (9.25 * (person.Weight / 2.205))) + (3.1 * heightCm) - (4.33 * person.Age))*intensity);
+                    break;
+                default:
+                    throw new ArgumentException();
+                    break;
+            }
+            Results.BMI = (person.Weight / (Math.Pow(((feet * 12) + inches), 2))) * 703;
+            Results.carbs = (int)Math.Round((Results.Maintenance_Calories * .55) / 4);
+            Results.fat = (int)Math.Round((Results.Maintenance_Calories * .275) / 9);
+            Results.protein = (int)Math.Round(person.Weight * .8);
+
+            return Results;
+
+        }
+    }
+
+}
